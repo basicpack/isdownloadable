14997d3d40b481f44f1fc2d7e3ea68b09369fe22
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-12 16:10:16 -0300

    D  test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'isdownloadable.php';
-$url='https://cio.com.br/RSS2/';
-if(isDownloadable($url)){
-    print $url.' é descarregável'.PHP_EOL;
-}else{
-    print $url.' não é descarregável'.PHP_EOL;
-}
