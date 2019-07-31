a76f6fd4aabb3bfaedc83035f3d195c762a409c3
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-07-31 09:36:03 -0300

    M  isdownloadable.php

--- isdownloadable.php
+++ isdownloadable.php
@@ -4,13 +4,23 @@
 //https://tinyurl.com/y6m8vuvp
 
 function isDownloadable($url) {
-    $file_headers = @get_headers($url);
-    if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
-        $exists = false;
+    $userAgent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0';
+    $tempoMaximoEmSegundos=10;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER  , true);  // we want headers
+    curl_setopt($ch, CURLOPT_NOBODY  , true);  // we don't need body
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $tempoMaximoEmSegundos);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+    $result = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if($httpCode==200){
+        return true;
+    }else{
+        return false;
     }
-    else {
-        $exists = true;
-    }
-    return $exists;
 }
 
